837a7bdeea280b49044d5bb4acbcc0c9d5b27901
Stanislaw Makarchuk <49611600+StasMakarchuk@users.noreply.github.com>
2025-09-24 12:01:09 +0100

    Update and rename test_lsf.sh to run_module_lsf.sh

---
+++ optional/nextflow/modules/register_multimodal_palom/run_module_lsf.sh
@@ -0,0 +1,3 @@
+nextflow run main.nf \
+  -profile lsf_singularity \
+  --samplesheet "paths.txt" \
--- optional/nextflow/modules/register_multimodal_palom/test_lsf.sh
+++
@@ -1,4 +0,0 @@
-nextflow run main.nf \
-  -profile lsf_singularity \
-  --samplesheet "/lustre/scratch127/cellgen/cellgeni/sm57/hackathon_0925/module_image_alignment/nf-module/test_paths_2.txt" \
-  -process.clusterOptions '-G team283'
